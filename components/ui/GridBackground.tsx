aba35602c60c1558cb76d4de4ea6f71a6887f850
bosTM-code <82539190+bosTM-code@users.noreply.github.com>
2025-09-19 22:47:14 +0200

    Hero Section Created

---
+++ components/ui/GridBackground.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+export function GridBackground() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-white dark:bg-black-100 absolute top-0 left-0">
+      <div
+        className={cn(
+          "absolute inset-0",
+          "[background-size:80px_80px]",
+          "[background-image:linear-gradient(to_right,#e4e4e7_0.03,transparent_1px),linear-gradient(to_bottom,#e4e4e7_0.03,transparent_1px)]",
+          "dark:[background-image:linear-gradient(to_right,#262626_0.03,transparent_1px),linear-gradient(to_bottom,#262626_0.03,transparent_1px)]"
+        )}
+      />
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
+    </div>
+  );
+}
